ac2c22aa0aa6995dc43260b387ed44ab377698d7
Marcello Romani <marcello.romani@ocyan.com>
2020-12-29 19:27:06 +0000

    added userdata for ec2 instance used for reverse ssh port forwarding

---
+++ ec2_userdata.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#
+# Update system
+#
+
+# amazon linux 2
+yum update -y
+
+#
+# Update DNS record
+#
+
+RECORD="astropi"
+ZONE="marcelloromani.name"
+ZONE_ID="Z2QQXSV9NP7LNN"
+
+IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "PUBLIC IP = ${IP}"
+CHANGES=$(cat <<EOF
+{
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "${RECORD}.${ZONE}",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [
+                    {
+                        "Value": "${IP}"
+                    }
+                ]
+            }
+        }
+    ]
+}
+EOF
+)
+
+echo "$CHANGES"
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch "$CHANGES"
+
+#
+# Update SSHD settings to expose forwarded ports
+#
+
+sed -i 's/#GatewayPorts/GatewayPorts/'       /etc/ssh/sshd_config
+sed -i 's/GatewayPorts no/GatewayPorts yes/' /etc/ssh/sshd_config
+
